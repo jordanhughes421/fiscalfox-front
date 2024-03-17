f93140d76762ebc23b47e3362f85ae1f740518e7
jordanhughes421 <jordanhughes421@gmail.com>
2024-03-17 19:46:24 -0400

    adjusted navbar to be responzive

--- src/Components/Navbar/navbar.js
+++ src/Components/Navbar/navbar.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Outlet, NavLink } from "react-router-dom";
+import React, { useState } from 'react';
+import { NavLink } from "react-router-dom";
 import { useAuth } from '../auth/AuthContext';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
@@ -7,6 +7,10 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
+import IconButton from '@mui/material/IconButton';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import MenuIcon from '@mui/icons-material/Menu';
 
 // Custom styling for the active link
 const activeStyle = {
@@ -15,44 +19,105 @@ const activeStyle = {
 
 function Navbar() {
     const { isLoggedIn, logout } = useAuth();
+    const [anchorElNav, setAnchorElNav] = useState(null);
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
 
     return (
-        <Box sx={{ flexGrow: 1 }}>
-            <AppBar position="static">
-                <Container maxWidth="xl">
-                    <Toolbar disableGutters>
-                        <Typography
-                            variant="h6"
-                            noWrap
-                            component="div"
-                            sx={{ flexGrow: 1, display: 'flex', alignItems: 'center' }}
+        <AppBar position="static">
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="div"
+                        sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}
+                    >
+                        Fiscal Fox
+                    </Typography>
+
+                    <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit"
                         >
-                            Fiscal Fox
-                        </Typography>
-                        <Box sx={{ display: 'flex', flexGrow: 1, justifyContent: 'flex-end' }}>
-                            <Button color="inherit" component={NavLink} to="/" end style={({ isActive }) => isActive ? activeStyle : undefined}>
-                                Home
-                            </Button>
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: { xs: 'block', md: 'none' },
+                            }}
+                        >
+                            {/* Mobile menu items */}
+                            <MenuItem onClick={handleCloseNavMenu} component={NavLink} to="/" style={({ isActive }) => isActive ? activeStyle : undefined}>
+                                <Typography textAlign="center">Home</Typography>
+                            </MenuItem>
                             {!isLoggedIn && (
                                 <>
-                                    <Button color="inherit" component={NavLink} to="/register" style={({ isActive }) => isActive ? activeStyle : undefined}>
-                                        Register
-                                    </Button>
-                                    <Button color="inherit" component={NavLink} to="/login" style={({ isActive }) => isActive ? activeStyle : undefined}>
-                                        Login
-                                    </Button>
+                                    <MenuItem onClick={handleCloseNavMenu} component={NavLink} to="/register" style={({ isActive }) => isActive ? activeStyle : undefined}>
+                                        <Typography textAlign="center">Register</Typography>
+                                    </MenuItem>
+                                    <MenuItem onClick={handleCloseNavMenu} component={NavLink} to="/login" style={({ isActive }) => isActive ? activeStyle : undefined}>
+                                        <Typography textAlign="center">Login</Typography>
+                                    </MenuItem>
                                 </>
                             )}
                             {isLoggedIn && (
-                                <Button color="inherit" onClick={logout}>Logout</Button>
+                                <MenuItem onClick={() => {
+                                    handleCloseNavMenu();
+                                    logout();
+                                }}>
+                                    <Typography textAlign="center">Logout</Typography>
+                                </MenuItem>
                             )}
-                        </Box>
-                    </Toolbar>
-                </Container>
-            </AppBar>
-            <Outlet />
-        </Box>
+                        </Menu>
+                    </Box>
 
+                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, justifyContent: 'flex-end' }}>
+                        {/* Desktop menu items */}
+                        <Button color="inherit" component={NavLink} to="/" end style={({ isActive }) => isActive ? activeStyle : undefined}>
+                            Home
+                        </Button>
+                        {!isLoggedIn && (
+                            <>
+                                <Button color="inherit" component={NavLink} to="/register" style={({ isActive }) => isActive ? activeStyle : undefined}>
+                                    Register
+                                </Button>
+                                <Button color="inherit" component={NavLink} to="/login" style={({ isActive }) => isActive ? activeStyle : undefined}>
+                                    Login
+                                </Button>
+                            </>
+                        )}
+                        {isLoggedIn && (
+                            <Button color="inherit" onClick={logout}>Logout</Button>
+                        )}
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
     );
 }
 
